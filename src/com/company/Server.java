b81b12e72db39a40d95d2eec82d07b1820cc33b7
Mekael Wasti <mekael_wasti@hotmail.com>
2022-04-12 04:44:18 -0400

    Added direct messaging

--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -11,10 +11,15 @@ public class Server {
     private BufferedWriter bw;
     //    public String message = null;
     public String message = "";
+    public String finalMessage = "";
+    public String address = "";
     public Socket s;
     public ArrayList<Socket> listOfClients = new ArrayList<Socket>();
+    public String recipientClient = "";
 
     public void start() throws IOException {
+        //Socket
+
         //Socket
 
         ServerSocket ss = new ServerSocket(63030);
@@ -36,7 +41,7 @@ public class Server {
                 byte[] ip4bytes = in4address.getAddress(); // returns byte[4]
                 String ip4string = in4address.toString();
                 Socket finalS = s;
-                System.out.println("Client Connected: " + ip4string);
+                System.out.println("Client Connected: " + s.getRemoteSocketAddress());
 
                 Thread thread1 = new Thread(() -> {
                     count.getAndIncrement();
@@ -45,15 +50,30 @@ public class Server {
 
 
                         BufferedReader br = null;
-
                         try {
+
+
                             br = new BufferedReader(new InputStreamReader(finalS.getInputStream()));
                             bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+                            String line;
+                            int i = 0;
+                            while ((i != 2)) {
+                                line = br.readLine();
+                                if (i == 0) {
+                                    address = line;
+                                }
+                                else {
+                                    finalMessage = line;
+                                }
+                                i++;
+                            }
 
-                            message = br.readLine();
-                            System.out.println("Message is: " + message);
-                            writeMessage(finalS);
-                            System.out.println(this.message);
+                            System.out.println("Sending: " + finalMessage + " - To: " + address);
+                            writeMessage(finalS, address);
+//                            System.out.println("Message is: " + finalMessage);
+//                            System.out.println(this.finalMessage);
+                            address = "";
+                            finalMessage = "";
 
                         } catch (IOException e) {
                             removeClient(finalS);
@@ -72,21 +92,26 @@ public class Server {
                         }
                     }
                 });
-
                 thread1.start();
             }
         });
         thread.start();
     }
 
-    public void writeMessage(Socket s) throws IOException {
+    public void writeMessage(Socket s, String address) throws IOException {
+        System.out.println("Address: " + address);
+        System.out.println("Address: " + s.getRemoteSocketAddress());
+
         try {
+
             //Send to each client
+
             for (int i = 0; i < listOfClients.size(); i++) {
-                System.out.println(listOfClients.get(i).getRemoteSocketAddress());
-                if (!Objects.equals(listOfClients.get(i).getRemoteSocketAddress().toString(), s.getRemoteSocketAddress().toString())) {
+
+//                System.out.println(listOfClients.get(i).getRemoteSocketAddress());
+                if (Objects.equals(listOfClients.get(i).getRemoteSocketAddress().toString(), address)) {
                     bw = new BufferedWriter(new OutputStreamWriter(listOfClients.get(i).getOutputStream()));
-                    bw.write(message);
+                    bw.write(finalMessage);
                     bw.newLine();
                     bw.flush();
                 }
